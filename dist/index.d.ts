b967edc7d0027d2189c760b22d3ac87344fa60c3
Paweł Jaśpiński <pawel.jaspinski@gmail.com>
2022-05-23 00:14:29 +0200

    TC-63 part 2

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,11 @@
-import { PluginDefinition } from 'sequences-types';
-declare const plugin: PluginDefinition;
-export default plugin;
+/// <reference types="node" />
+import { Input, PluginTemplate, PluginSettings } from 'sequences-types';
+import net from 'net';
+declare class VMixPlugin extends PluginTemplate {
+    name: string;
+    settingsFields: Input[];
+    socket: net.Socket;
+    setup: (options: PluginSettings) => void;
+    destroy: () => void;
+}
+export default VMixPlugin;
--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,33 @@
+import { PluginTemplate } from 'sequences-types';
 import net from 'net';
-const name = 'VMix';
-const settingsFields = [
-    {
-        type: 'TEXT',
-        id: 'ip',
-        required: true,
-        value: '127.0.0.1',
-        regex: '^[0-9]+.[0-9]+.[0-9]+.[0-9]+$',
-        label: 'IP address',
-    },
-    {
-        type: 'TEXT',
-        id: 'port',
-        required: true,
-        value: '8099',
-        regex: '^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$',
-        label: "Port (fixed in VMix, so you probably don't want to change this)",
-    },
-];
-const socket = new net.Socket();
-const setup = (options) => {
-    const { ip, port } = options;
-    socket.connect(+port, ip, () => console.log('Connected to a VMix instance'));
-};
-const destroy = () => {
-    socket.end();
-    socket.destroy();
-};
-const plugin = { name, settingsFields, setup, destroy };
-export default plugin;
+class VMixPlugin extends PluginTemplate {
+    name = 'VMix';
+    settingsFields = [
+        {
+            type: 'TEXT',
+            id: 'ip',
+            required: true,
+            value: '127.0.0.1',
+            regex: '^[0-9]+.[0-9]+.[0-9]+.[0-9]+$',
+            label: 'IP address',
+        },
+        {
+            type: 'TEXT',
+            id: 'port',
+            required: true,
+            value: '8099',
+            regex: '^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$',
+            label: "Port (fixed in VMix, so you probably don't want to change this)",
+        },
+    ];
+    socket = new net.Socket();
+    setup = (options) => {
+        const { ip, port } = options;
+        this.socket.connect(+port, ip, () => console.log('Connected to a VMix instance'));
+    };
+    destroy = () => {
+        this.socket.end();
+        this.socket.destroy();
+    };
+}
+export default VMixPlugin;
