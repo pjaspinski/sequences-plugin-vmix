d7c2d19c8c466de2c29b806877a65ac85cb59b3c
Paweł Jaśpiński <pawel.jaspinski@gmail.com>
2022-05-22 23:58:02 +0200

    TC-63

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,38 @@
-import { Input, PluginDefinition, PluginSettings, PluginStatus } from 'sequences-types';
+import { Input, PluginTemplate, PluginSettings } from 'sequences-types';
 import net from 'net';
 
-const name = 'VMix';
-const settingsFields: Input[] = [
-	{
-		type: 'TEXT',
-		id: 'ip',
-		required: true,
-		value: '127.0.0.1',
-		regex: '^[0-9]+.[0-9]+.[0-9]+.[0-9]+$',
-		label: 'IP address',
-	},
-	{
-		type: 'TEXT',
-		id: 'port',
-		required: true,
-		value: '8099',
-		regex: '^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$',
-		label: "Port (fixed in VMix, so you probably don't want to change this)",
-	},
-];
+class VMixPlugin extends PluginTemplate {
+	name = 'VMix';
+	settingsFields: Input[] = [
+		{
+			type: 'TEXT',
+			id: 'ip',
+			required: true,
+			value: '127.0.0.1',
+			regex: '^[0-9]+.[0-9]+.[0-9]+.[0-9]+$',
+			label: 'IP address',
+		},
+		{
+			type: 'TEXT',
+			id: 'port',
+			required: true,
+			value: '8099',
+			regex: '^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$',
+			label: "Port (fixed in VMix, so you probably don't want to change this)",
+		},
+	];
+	socket = new net.Socket();
 
-const socket = new net.Socket();
+	setup = (options: PluginSettings) => {
+		const { ip, port } = options;
 
-const setup = (options: PluginSettings) => {
-	const { ip, port } = options;
+		this.socket.connect(+port, ip, () => console.log('Connected to a VMix instance'));
+	};
 
-	socket.connect(+port, ip, () => console.log('Connected to a VMix instance'));
-};
+	destroy = () => {
+		this.socket.end();
+		this.socket.destroy();
+	};
+}
 
-const destroy = () => {
-	socket.end();
-	socket.destroy();
-};
-
-const plugin: PluginDefinition = { name, settingsFields, setup, destroy };
-
-export default plugin;
+export default VMixPlugin;
